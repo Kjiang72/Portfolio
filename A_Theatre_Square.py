aa4d1ac32e8e6b1fa9511cdd07ae4a336f04e219
Kjiang72 <kevinjiang678@gmail.com>
2023-06-09 21:37:27 -0700

    accidentally pushed some garbage

--- A_Theatre_Square.py
+++
@@ -1,5 +0,0 @@
-n, m, a = list(map(int, input().split()))
-
-vert = m//a + 1 if m % a != 0 else m//a
-hor = n//a + 1 if n % a != 0 else n//a
-print(vert * hor)
--- B_Vanya_and_Lanterns.py
+++
@@ -1,8 +0,0 @@
-n, l = list(map(int, input().split()))
-arr = list(map(int, input().split()))
-arr.sort()
-rad = max(arr[0] - 0, l - arr[-1])
-for i in range(1, len(arr)):
-  if arr[i] - rad > arr[i - 1] + rad:
-    rad = (arr[i] - arr[i - 1]) / 2
-print("%.10f" % rad)
